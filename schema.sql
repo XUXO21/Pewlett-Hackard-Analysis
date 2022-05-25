e77f7fae876ab169ebff49196e2d78cfdb9b39a4
XUXO21 <jesus.mz@hotmail.com>
2022-05-24 22:27:03 -0500

    Tables Created

--- schema.sql
+++ schema.sql
@@ -7,22 +7,13 @@ CREATE TABLE departments (
 );
 
 CREATE TABLE employees (
-	 emp_no INT NOT NULL,
+	emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
      first_name VARCHAR NOT NULL,
      last_name VARCHAR NOT NULL,
      gender VARCHAR NOT NULL,
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
-);
-
-CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
@@ -35,9 +26,18 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  PRIMARY KEY (emp_no)
+);
+
 CREATE TABLE dept_emp (
-dept_no VARCHAR(4) NOT NULL,
-    emp_no INT NOT NULL,
+emp_no INT NOT NULL,
+	dept_no VARCHAR NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -47,11 +47,17 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
+  title VARCHAR(18) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (emp_no) REFERENCES salaries (emp_no),
-	PRIMARY KEY (emp_no)
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no, from_date)
 );
 
+-- Query for Confirmation
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM dept_manager
+SELECT * FROM salaries
+SELECT * FROM titles
+SELECT * FROM dept_emp
